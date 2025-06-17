2b7ef91c65b76dce99f7744ce4d5f41f0a671088
NilsPvR <nilspvr@pm.me>
2025-06-17 23:24:05 +0200

    feat: add first colour palette + usage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container mx-auto p-4 bg-background-800 text-white">
+    <div class="p-5 bg-background-700">
+        <h1>Welcome to SvelteKit</h1>
+    </div>
+    <p >Visit <a class="text-primary-500" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <div class="w-20 h-20 bg-primary-500"></div>
+    <div class="w-20 h-20 bg-accent-500"></div>
+    <div class="w-20 h-20 bg-secondary-500"></div>
+</div>
