c54b4b43306a7192335325b8ce7f671b5fac58c8
keno.albers <git@kenoalbers.de>
2025-06-18 22:22:36 +0200

    feat: add atma font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1 class="text-4xl font-bold">Terralux</h1>
-<p class="mt-4 text-lg">Die smarte Lampensteuerung für dein Reptil.</p>
+<h1 class="text-4xl font-atma font-bold">Terralux</h1>
+<p class="mt-4 text-xl font-atma font-medium">Die smarte Lampensteuerung für dein Reptil.</p>
 
 <div class="bg-primary-500 h-20 w-20"></div>
 <div class="bg-accent-500 h-20 w-20"></div>
