13e50fe9bfa8f4f0fdfad28e72c0a4459ccca10e
keno.albers <git@kenoalbers.de>
2025-06-19 08:48:40 +0200

    feat: center a div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<h1 class="text-4xl font-atma font-bold">Terralux</h1>
-<p class="mt-4 text-xl font-atma font-medium">Die smarte Lampensteuerung für dein Reptil.</p>
+<div class="flex justify-center">
+    <h1 class="text-6xl font-atma font-bold">Terralux</h1>
+</div>
+
+<p class="mt-4 text-3xl font-atma font-medium">Die smarte <bold class="font-bold">all-in-one</bold> Lampensteuerung für dein Reptil.</p>
 
 <div class="bg-primary-500 h-20 w-20"></div>
 <div class="bg-accent-500 h-20 w-20"></div>
