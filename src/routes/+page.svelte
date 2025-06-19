3e8a4e84c0522c6ff2e26f36e991ba67cc6299a1
keno.albers <git@kenoalbers.de>
2025-06-19 16:34:14 +0200

    feat: main page scaffold

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <div class="flex justify-center">
-    <h1 class="text-6xl font-atma font-bold">Terralux</h1>
+  <h1 class="text-6xl font-atma font-bold">Terralux</h1>
 </div>
 
-<p class="mt-4 text-3xl font-atma font-medium">Die smarte <bold class="font-bold">all-in-one</bold> Lampensteuerung für dein Reptil.</p>
+<div class="flex flex-col lg:flex-row mt-8 gap-8 px-4">
+  <div class="flex-1">
+    <p class="mt-4 text-3xl font-atma font-medium">
+      Die smarte <strong class="font-bold">all-in-one</strong> Lampensteuerung für dein Reptil.
+    </p>
+    <div class="flex justify-center mt-6">
+      <button class="text-3xl bg-primary-600 hover:bg-primary-700 text-white font-bold py-3 px-6 rounded">
+        Jetzt starten
+      </button>
+    </div>
+  </div>
+
+ <div class="flex-1 flex justify-center items-center">
+  <img src="image/Reptiel_Auf_Ast.png" alt="Reptil auf Ast" class="h-63 rounded object-cover" />
+</div> 
+</div>
 
 <div class="bg-primary-500 h-20 w-20"></div>
 <div class="bg-accent-500 h-20 w-20"></div>
