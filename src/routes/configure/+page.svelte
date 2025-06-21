09701cd9d12f59e8f10c5e465c123fb7a20cb775
NilsPvR <nilspvr@pm.me>
2025-06-21 23:24:03 +0200

    feat: add second map

--- src/routes/configure/+page.svelte
+++ src/routes/configure/+page.svelte
@@ -2,7 +2,28 @@
     import MapClickMarker from '$lib/components/locationPicker.svelte';
     import { LatLng } from 'leaflet';
 
-    let coords: LatLng | null = null;
+    let natCoords: LatLng | null = null;
+    let terrCoords: LatLng | null = null;
 </script>
 
-<MapClickMarker onSelect={(pos) => (coords = pos)} />
+<h1 class="mb-10 text-center text-6xl font-bold">Terralux Konfigurator</h1>
+
+<div class="flex flex-col items-center gap-4">
+    <div class="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
+        <div class="flex flex-col">
+            <h2 class="mb-2 text-center text-xl font-semibold">Matürlicher Standort</h2>
+            <div class="aspect-[6/5] w-full overflow-hidden shadow">
+                <MapClickMarker onSelect={(pos) => (natCoords = pos)} />
+            </div>
+        </div>
+
+        <div class="flex flex-col">
+            <h2 class="mb-2 text-center text-xl font-semibold">Terrarium Standort</h2>
+            <div class="aspect-[6/5] w-full overflow-hidden shadow">
+                <MapClickMarker onSelect={(pos) => (terrCoords = pos)} />
+            </div>
+        </div>
+    </div>
+
+    <p class="text-center">Klicke auf die Karten, um eine Position auszuwählen</p>
+</div>
