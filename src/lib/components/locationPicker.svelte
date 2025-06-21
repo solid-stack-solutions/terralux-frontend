9e2195fb1eb5d9953e9f4f24ba47657cb6d23f23
NilsPvR <nilspvr@pm.me>
2025-06-21 22:36:28 +0200

    feat: map tile layer with less noise

--- src/lib/components/locationPicker.svelte
+++ src/lib/components/locationPicker.svelte
@@ -15,9 +15,12 @@
     onMount(() => {
         map = L.map(mapDiv).setView(initialCenter, initialZoom);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors',
+        // see https://leaflet-extras.github.io/leaflet-providers/preview/ for list of available tile layers
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
             maxZoom: 8,
+            minZoom: 2,
         }).addTo(map);
 
         map.on('click', (e: L.LeafletMouseEvent) => {
