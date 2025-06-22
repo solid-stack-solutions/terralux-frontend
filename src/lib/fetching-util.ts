b694c2a128d8ad34e6a3090fc89049334f4cd3ec
Jannik <jannik.hennicke@proton.me>
2025-06-22 12:50:54 +0200

    code review suggestion: remove obsolete fetch with credentials

--- src/lib/fetching-util.ts
+++ src/lib/fetching-util.ts
@@ -10,43 +10,22 @@ async function tryFetching(
     url: string,
     method: string,
     body: string,
-    restricted: boolean,
     query: any,
 ) {
     if (query) {
         url = url + '?' + new URLSearchParams(query).toString();
     }
     return await retryPolicy.execute(async () => {
-        if (method === 'GET' || query || method === undefined) {
-            return await fetch(
-                new Request(url, {
-                    method: method ?? 'GET', // Default to GET
-                    credentials: restricted ? 'include' : 'omit',
-                }),
-            );
-        } else {
             return await fetch(
                 new Request(url, {
                     method: method,
-                    credentials: restricted ? 'include' : 'omit',
                     headers: {
                         'Content-Type': body === undefined ? 'text/plain' : 'application/json',
                     },
                     body: body ? JSON.stringify(body) : '',
                 }),
             );
-        }
     });
 }
 
-// Fetch with credentials
-async function tryFetchingRestricted(url: string, method: string, body: any, query: any) {
-    return await tryFetching(url, method, body, true, query);
-}
-
-// Fetch without credentials
-async function tryFetchingPublic(url: string, method: string, body: any, query: any) {
-    return await tryFetching(url, method, body, false, query);
-}
-
-export { tryFetchingPublic, tryFetchingRestricted };
+export { tryFetching };
