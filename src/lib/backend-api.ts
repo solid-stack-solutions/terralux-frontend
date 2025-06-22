d0c0f73d66d9755bf2e5509df6c2ccfef1754565
Janniix <53787111+Janniix@users.noreply.github.com>
2025-06-22 12:43:59 +0200

    Apply suggestion for src/lib/backend-api.ts
    Co-authored-by: Nils van Rijsinge <nilspvr@pm.me>

--- src/lib/backend-api.ts
+++ src/lib/backend-api.ts
@@ -2,10 +2,10 @@ import { tryFetchingPublic } from './fetching-util';
 import { getBackendUrl, endpoints } from './backend-util';
 /**
  * Requests to change the power state of the plug
- * @param {boolean} power the power state the plug should be switched to
- * @returns {Promise<Response>} the backend response as a {@link Promise<Response>}. Consult backend API documentation for response code semantics (https://github.com/solid-stack-solutions/terralux-backend)
+ * @param power the power state the plug should be switched to
+ * @returns the backend response as a {@link Promise<Response>}. Consult backend API documentation for response code semantics (https://github.com/solid-stack-solutions/terralux-backend)
  */
-async function changePlugState(power: boolean) {
+async function changePlugState(power: boolean): Promise<Response> {
     return await tryFetchingPublic(getBackendUrl() + endpoints.put.power_state, 'PUT', undefined, {
         power: power,
     });
