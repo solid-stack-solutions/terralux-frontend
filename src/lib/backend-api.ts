a177635c38c817f3709368be042eb69f2a9e61ad
Jannik <jannik.hennicke@proton.me>
2025-06-22 11:11:58 +0200

    feat: add plug state change backend request

---
+++ src/lib/backend-api.ts
@@ -0,0 +1,12 @@
+import { tryFetchingPublic } from "./fetching-util";
+import { getBackendUrl, endpoints } from "./backend-util";
+/**
+ * Requests to change the power state of the plug
+ * @param {boolean} power the power state the plug should be switched to
+ * @returns {Promise<Response>} the backend response as a {@link Promise<Response>}. Consult backend API documentation for response code semantics (https://github.com/solid-stack-solutions/terralux-backend)
+ */
+async function changePlugState(power:boolean) {
+    return await tryFetchingPublic(getBackendUrl + endpoints.put.power_state, "PUT", undefined, {power: power});
+}
+
+export {changePlugState};
