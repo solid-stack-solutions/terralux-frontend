1c2890c676bc5100761d8cbc3b774ed5d47f0355
Jannik <jannik.hennicke@proton.me>
2025-06-27 18:25:57 +0200

    style: run format

--- src/lib/backend-api.ts
+++ src/lib/backend-api.ts
@@ -1,6 +1,6 @@
 import { tryFetching, HTTP_METHOD } from './fetching-util';
 import { getBackendUrl, endpoints } from './backend-util';
-import type { PlugConfiguration, PlugTimer, PowerState  } from './data-types';
+import type { PlugConfiguration, PlugTimer, PowerState } from './data-types';
 
 /**
  * Requests to change the power state of the plug
@@ -18,12 +18,9 @@ async function changePlugState(power: boolean): Promise<Response> {
  * @returns the backend response as a {@link Promise<PowerState>}. Consult backend API documentation for response code semantics (https://github.com/solid-stack-solutions/terralux-backend)
  */
 async function getPlugState(): Promise<PowerState> {
-    return await (await tryFetching(
-        getBackendUrl() + endpoints.get.power_state,
-        HTTP_METHOD.GET,
-        null,
-        '',
-    )).json();
+    return await (
+        await tryFetching(getBackendUrl() + endpoints.get.power_state, HTTP_METHOD.GET, null, '')
+    ).json();
 }
 
 /**
