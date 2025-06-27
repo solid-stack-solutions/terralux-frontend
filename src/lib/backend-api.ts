39838d099649dc214d3d494114896238c2023e02
Jannik <jannik.hennicke@proton.me>
2025-06-27 12:30:25 +0200

    feat: add configuration put

--- src/lib/backend-api.ts
+++ src/lib/backend-api.ts
@@ -1,5 +1,6 @@
 import { tryFetching, HTTP_METHOD } from './fetching-util';
 import { getBackendUrl, endpoints } from './backend-util';
+import type { PlugConfiguration } from './response-types';
 /**
  * Requests to change the power state of the plug
  * @param power the power state the plug should be switched to
@@ -10,5 +11,14 @@ async function changePlugState(power: boolean): Promise<Response> {
         power: power,
     });
 }
+
+/**
+ * Requets top change the configuration of the plug
+ * @param configuration the new configuration
+ * @returns the backend response as a {@link Promise<Response>}. Consult backend API documentation for response code semantics (https://github.com/solid-stack-solutions/terralux-backend)
+ */
+async function changeConfiguration(configuration:PlugConfiguration): Promise<Response> {
+    return await tryFetching(getBackendUrl() + endpoints.put.configuration, HTTP_METHOD.PUT, JSON.stringify(configuration), null);
+};
 
 export { changePlugState };
---
+++ src/lib/response-types.ts
@@ -0,0 +1,8 @@
+export type PlugConfiguration = {
+    plug_url:string,
+    natural_factor: number,
+    local_latitude: number,
+    local_longitude: number,
+    natural_latitude: number,
+    natural_longitude: number
+};
