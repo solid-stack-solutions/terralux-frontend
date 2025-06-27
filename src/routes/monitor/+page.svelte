2ac806f1c0cda0c7b3de69964dfe918a9b060e48
Jannik <jannik.hennicke@proton.me>
2025-06-27 14:44:36 +0200

    feat: move cluster above graph

--- src/routes/monitor/+page.svelte
+++ src/routes/monitor/+page.svelte
@@ -5,20 +5,17 @@
 
 <section class="mx-auto max-w-5xl">
   <div class="flex flex-col justify-center">
+    
       <h1 class="text-center text-6xl font-bold">Terralux Kontrollstation</h1>
-
+    <div class="flex justify-center">
+        <ManualOnOffCluster background_styles="w-128"/>
+    </div>
       <MonitorChart />
       <div class="flex w-full justify-center">
-          <div class="flex max-w-fit flex-col gap-y-4">
-              <div class="flex justify-center">
-                  <ManualOnOffCluster background_styles="bg-primary-500" />
-              </div>
-
               <div class="flex justify-center">
                   <button type="button" class="btn preset-filled-primary-500 w-128"
                       >Neu konfigurieren</button
                   >
-              </div>
           </div>
       </div>
   </div>
