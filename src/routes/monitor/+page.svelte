b276830c621747a7e4a0b961adf16ef1ece028a4
Jannik <jannik.hennicke@proton.me>
2025-06-27 14:44:51 +0200

    style: run format

--- src/routes/monitor/+page.svelte
+++ src/routes/monitor/+page.svelte
@@ -4,19 +4,18 @@
 </script>
 
 <section class="mx-auto max-w-5xl">
-  <div class="flex flex-col justify-center">
-    
-      <h1 class="text-center text-6xl font-bold">Terralux Kontrollstation</h1>
-    <div class="flex justify-center">
-        <ManualOnOffCluster background_styles="w-128"/>
+    <div class="flex flex-col justify-center">
+        <h1 class="text-center text-6xl font-bold">Terralux Kontrollstation</h1>
+        <div class="flex justify-center">
+            <ManualOnOffCluster background_styles="w-128" />
+        </div>
+        <MonitorChart />
+        <div class="flex w-full justify-center">
+            <div class="flex justify-center">
+                <button type="button" class="btn preset-filled-primary-500 w-128"
+                    >Neu konfigurieren</button
+                >
+            </div>
+        </div>
     </div>
-      <MonitorChart />
-      <div class="flex w-full justify-center">
-              <div class="flex justify-center">
-                  <button type="button" class="btn preset-filled-primary-500 w-128"
-                      >Neu konfigurieren</button
-                  >
-          </div>
-      </div>
-  </div>
 </section>
